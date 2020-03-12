9e2fa33fa19b926a77ec874d8e9c804a2ef85dac
33daysK <enjingorira@gmail.com>
2020-03-12 18:21:12 +0900

    一番最初に作ったファイルです

---
+++ CsvFileWrite.kt
@@ -0,0 +1,32 @@
+package csv
+/*
+ * ファイルに書き込みをしています
+ * .use関数がJavaで言うtry-with-resorceの役割を果たしてくれているため自動的にcloseされます。
+ */
+import java.io.File
+
+fun csvFileWrite (fileName: String, sorted_StudentList: List<Student>){
+
+	var record :String
+	printScreen(sorted_StudentList)
+
+	// 新しいファイルを作ってＣＳＶ形式にするためにカンマを挟み込んでいます。
+	File(fileName).bufferedWriter().use { out ->
+		sorted_StudentList.forEach{
+			record = it.number + ","
+			record += it.name + ","
+			record += it.kurasu + ","
+			record += it.age.toString() + ","
+			record += it.score.toString() + ","
+			out.write(record)
+			out.newLine()  // 改行
+		}
+	}
+}
+
+
+fun printScreen(sorted_studentList: List<Student>){
+	println("%3s%8s%20s%3s%3s".format("番号","名前","クラス","年齢","点数"))
+	sorted_studentList.forEach{
+		println("%-8s%-${25-it.name.length}s%5s%6d%5d".format(it.number,it.name,it.kurasu,it.age,it.score))}
+}
